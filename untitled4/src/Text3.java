4b652586bed646e2609ef0c75e0b7fe6cebe1b27
hizgsnbu <hutanke571964243@126.com>
2021-03-13 11:50:21 +0800

    shujuleixing2

---
+++ untitled4/src/Text3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Text3 {
+    public static void main(String[] args) {
+        System.out.println("请输入一个整数：");
+        Scanner sc = new Scanner(System.in);
+//        键盘输入的的数保存在ｎｕｍ
+        int num = sc.nextInt();
+       /*if(num>3 & num<6){
+           System.out.println("true");
+       }
+       else {
+           System.out.println("false");
+       }
+*/
+
+}
