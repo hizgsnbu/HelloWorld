8151e0e97250ae19316c3b91303956b3b8cde322
hizgsnbu <hutanke571964243@126.com>
2021-03-13 09:45:11 +0800

    shujuleixing

--- untitled1/src/shujuleixing.java
+++ untitled1/src/shujuleixing.java
@@ -25,5 +25,17 @@ public class shujuleixing {
         System out.println("整型char变量a_byte的值为："+a2_char"");
         System out.println("整型char变量a_int的值为："+a3_char"");
         System out.println("整型布尔型变量a_int的值为："+a_int"");
+        int a=298;
+        byte b;
+//    从大到小　需要强制类型转换
+        b=(byte) a;
+        System.out.println("a="+a);
+//    强制类型转换造成精度丢失
+        System.out.println("b="+b);
+
+        byte a1=3;
+        byte a2=4;
+        byte a3=a1+a2;
+        System.out.println("a3="+a3);
     }
 }
